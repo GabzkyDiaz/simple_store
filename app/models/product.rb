acc57ab7ce81894ba70fa4848898b7ae7d577f2d
GabzbyDiaz <gabzky.diaz@gmail.com>
2024-06-12 14:36:48 -0500

    Add validations to Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+
+  # app/models/product.rb
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :stock_quantity, presence: true
 end
