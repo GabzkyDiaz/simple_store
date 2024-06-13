ccf6e7b28a9d71ac71b8867c364c6517103a1ceb
GabzbyDiaz <gabzky.diaz@gmail.com>
2024-06-12 23:05:38 -0500

    Update seed script to load data from CSV

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,22 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-# db/seeds.rb
-require 'faker'
+require 'csv'
 
 Product.destroy_all
+Category.destroy_all
 
-676.times do
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true, encoding: 'iso-8859-1')
+
+products.each do |product|
+  category_name = product['category']
+  category = Category.find_or_create_by(name: category_name)
+
   Product.create(
-    title: Faker::Commerce.product_name,
-    description: Faker::Lorem.paragraph,
-    price: Faker::Commerce.price(range: 0..100.0),
-    stock_quantity: Faker::Number.between(from: 1, to: 100)
+    title: product['name'],
+    description: product['description'],
+    price: product['price'],
+    stock_quantity: product['stock quantity'],
+    category: category
   )
 end
